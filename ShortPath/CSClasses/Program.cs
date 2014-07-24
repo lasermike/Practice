0760487d27d87cb1ed55f574bc32e46b26c97c01
lasermike <mmatsel@MATSEL1.redmond.corp.microsoft.com>
2014-07-24 05:46:09 -0700

    cs bug

--- ShortPath/CSClasses/Program.cs
+++ ShortPath/CSClasses/Program.cs
@@ -25,6 +25,9 @@ namespace CSClasses
             graph.Add("CS402", new string[] { "CS302" });
             graph.Add("CS403", new string[] { "CS302" });
             graph.Add("CS500", new string[] { "CS402", "CS403" } );
+
+            graph.Solve();
+            Console.ReadKey();
         }
     }
 
@@ -53,13 +56,80 @@ namespace CSClasses
             count = 0;
         }
 
-
         public void Add(string className, string[] prereqs)
         {
-            classes.Add(className, new ClassNode(className, prereqs));
+            ClassNode existing;
+            if (classes.TryGetValue(className, out existing))
+            {
+                if (existing.prereqs.Length > 0)
+                    Array.Copy(prereqs, existing.prereqs, prereqs.Length);
+                else
+                    existing.prereqs = prereqs;
+            }
+            else
+            {
+                classes.Add(className, new ClassNode(className, prereqs));
+            }
         }
 
-        public string Solve()
+        bool AllTaken(string[] prereqs, Dictionary<string,bool> taken)
+        {
+            foreach (string pre in prereqs)
+            {
+                bool test;
+                if (!taken.TryGetValue(pre, out test) || !test)
+                    return false;
+            }
+            return true;
+        }
+
+        public void Solve()
+        {
+            Graph rGraph = this.ReverseGraph();
+            rGraph.DFS();
+            ClassNode[] ordered = rGraph.classes.OrderBy(item => item.Value.postCount).Select(item => item.Value).ToArray();
+            Console.WriteLine(ordered.Print());
+
+            // Groups
+            Console.WriteLine("Groupings");
+            Dictionary<string, bool> taken = new Dictionary<string, bool>(ordered.Length);
+            int groupNum = 1;
+
+            for (int i = 0; i < ordered.Length;i++)
+            {
+                taken[ordered[i].name] = true;
+                Console.WriteLine(ordered[i].name + "("+groupNum+")");
+
+                while (true)
+                {
+                    i++;
+                    if (i >= ordered.Length || !AllTaken(ordered[i].prereqs, taken))
+                    {
+                        groupNum++;
+                        i--;
+                        break;
+                    }
+                    Console.WriteLine(ordered[i].name + "(" + groupNum + ")");
+                }
+            }
+        }
+
+        private Graph ReverseGraph()
+        {
+            Graph rGraph = new Graph();
+            foreach (var cls in classes)
+            {
+                foreach (var prereq in cls.Value.prereqs)
+                {
+                    rGraph.Add(prereq, new string[] { cls.Key });
+                }
+                rGraph.Add(cls.Key, new string[] { } );
+            }
+
+            return rGraph;
+        }
+
+        private void DFS()
         {
             Stack<string> stack = new Stack<string>();
             foreach (string key in classes.Keys) 
@@ -91,10 +161,19 @@ namespace CSClasses
                 }
                 
             }
-
-            return "";
         }
 
 
+    }
+
+    static class ExtensionClass
+    {
+        public static string Print(this ClassNode[] nodes)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(var node in nodes)
+                sb.Append(node.name + "("+node.postCount+")\n");
+            return sb.ToString();
+        }
     }
 }
